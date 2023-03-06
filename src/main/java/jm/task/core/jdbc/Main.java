8250416dd32cf67dba9490fd5de24142d5a9e6e6
Evgeniy Zorin <zorin.n7@bk.ru>
2023-03-06 18:52:19 +0300

    на проверку задание 1.1.5

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,18 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main extends Util {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        List<User> tempList = new ArrayList<>();
-        tempList.add(new User("Иван", "Иванов", (byte) 21));
-        tempList.add(new User("Анастасия", "Михайлова", (byte) 27));
-        tempList.add(new User("Сергей", "Стешин", (byte) 44));
-        tempList.add(new User("Евгений", "Зорин", (byte) 38));
-        tempList.forEach(x -> {
-            userService.saveUser(x.getName(), x.getLastName(), x.getAge());
-            System.out.format("Пользователь с именем – %s %s добавлен в базу данных%n", x.getName(), x.getLastName());
-        });
+        userService.saveUser("Иван", "Иванов", (byte) 21);
+        userService.saveUser("Анастасия", "Михайлова", (byte) 27);
+        userService.saveUser("Сергей", "Стешин", (byte) 44);
+        userService.saveUser("Евгений", "Зорин", (byte) 38);
         userService.getAllUsers().forEach(user -> System.out.println(user.toString()));
         userService.cleanUsersTable();
         userService.dropUsersTable();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public static final String URL = "jdbc:mysql://localhost:3306/mydbjpp1.1.4";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "root";
-
+    //подключение JDBC
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -18,5 +27,33 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+    //подключение Hibernate
+    public static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbjpp1.1.4");
+                properties.put(Environment.USER, "root");
+                properties.put(Environment.PASS, "root");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "");
+                Configuration configuration = new Configuration().setProperties(properties).addAnnotatedClass(User.class);
+                StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(standardServiceRegistry);
+                System.out.println("SessionFactory успешно создана");
+            } catch (HibernateException e) {
+                System.out.println("Ошибка создания SessionFactory");
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Используется ранее созданная SessionFactory");
+        }
+        return sessionFactory;
     }
 }
